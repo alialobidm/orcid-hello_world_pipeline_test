700b2bcec9552e1b95375711793c1183111d022d
Giles Westwood <giles.westwood@gmail.com>
2024-10-10 13:45:25 +0100

    test fix

--- src/test/java/com/example/HelloWorldTest.java
+++ src/test/java/com/example/HelloWorldTest.java
@@ -2,14 +2,19 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HelloWorldTest {
+
     @Test
     public void testSayHello() {
         HelloWorld helloWorld = new HelloWorld();
         assertEquals("Hello, World!", helloWorld.sayHello());
     }
 
-    // This assertion will fail
-    assertTrue(false, "This is a failing test to test the pipeline.");
+    @Test
+    public void testFailingAssertion() {
+        // This assertion will fail
+        assertTrue(false, "This is a failing test to test the pipeline.");
+    }
 }
